1ebaf15ca06d80f1a5a2623da8d5beea39db2f1d
flightcity <infocome@infocome-no-Mac-mini.local>
2015-08-19 20:52:49 +0900

    add test for test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
+  it "is valid with title, target_price, current_price, start_datetime and end_datetime" do
+    product = Product.new(
+      title: 'title',
+      target_price: 'target_price',
+      current_price: 'current_price',
+      start_datetime: 'start_datetime',
+      end_datetime: 'end_datetime'
+    )
+    expect(product).to be_valid
+  end
+  describe '#title' do
+    it 'should be presence' do
+      should validate_presence_of(:title)
+    end
+  end
 end
